451d7219d249ffcffab9d2ac4a351cefb0f20a43
Ahmed Mansour <elkomieahmed@gmail.com>
2026-05-15 05:08:01 +0300

    Add files via upload

---
+++ School System/School System1/SelectForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace School_System1
+{
+    public partial class SelectForm : Form
+    {
+        DatabaseHelper db = new DatabaseHelper();
+        string type;
+        public SelectForm(string tableType)
+        {
+            InitializeComponent();
+            type = tableType;
+            this.Load += SelectForm_Load;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void SelectForm_Load(object sender, EventArgs e)
+        {
+            if (type == "student")
+            {
+                dataGridView1.DataSource = db.GetStudents();
+            }
+            else if (type == "teacher")
+            {
+                dataGridView1.DataSource = db.GetTeachers();
+            }
+            else if (type == "course")
+            {
+                dataGridView1.DataSource = db.GetCourses();
+            }
+            else if (type == "exam_max")
+            {
+                dataGridView1.DataSource = db.GetExamMaxMarks();
+            }
+            else if (type == "course_min")
+            {
+                dataGridView1.DataSource = db.GetMinCreditHours();
+            }
+            else if (type == "student_max_age")
+            {
+                dataGridView1.DataSource = db.GetMaxAgeByAddress();
+            }
+            else if (type == "multi_student")
+            {
+                dataGridView1.DataSource = db.studentmultiplecourse();
+            }
+            else if (type == "teacher>30")
+            {
+                dataGridView1.DataSource = db.Teachercoursebiggerthan30();
+            }
+            else if (type == "marksmorethanaverage")
+            {
+                dataGridView1.DataSource = db.marksmorethanaverage();
+            }
+            else if (type == "enrollmentjoin")
+            {
+                dataGridView1.DataSource = db.enrollmentjoinstudent();
+            }
+            else if (type == "teacherjoinsection")
+            {
+                dataGridView1.DataSource = db.teacherjoinsection();
+            }
+            else if (type == "examjoinstudent")
+            {
+                dataGridView1.DataSource = db.examjoinstudent();
+            }
+        }
+
+    }
+}
---
+++ School System/School System1/adminPage.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace School_System1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 home = new Form1();
+            home.Show();
+
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SelectForm f = new SelectForm("student");
+            f.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SelectForm f = new SelectForm("teacher");
+            f.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SelectForm f = new SelectForm("course");
+            f.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SelectForm f = new SelectForm("exam_max");
+            f.Show();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            SelectForm f = new SelectForm("student_max_age");
+            f.Show();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            SelectForm f = new SelectForm("course_min");
+            f.Show();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            SelectForm f = new SelectForm("multi_student");
+            f.Show();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            SelectForm f = new SelectForm("teacher>30");
+            f.Show();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            SelectForm f = new SelectForm("marksmorethanaverage");
+            f.Show();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            SelectForm f = new SelectForm("enrollmentjoin");
+            f.Show();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            SelectForm f = new SelectForm("teacherjoinsection");
+            f.Show();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            SelectForm f = new SelectForm("examjoinstudent");
+            f.Show();
+        }
+    }
+}
